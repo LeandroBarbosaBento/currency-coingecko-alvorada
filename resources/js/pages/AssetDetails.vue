4303a7c5991286993efee0c94a56db394b4c0f20
leandro <leandro@pop-os.localdomain>
2025-08-17 19:23:04 -0300

    feat: create details page

---
+++ resources/js/pages/AssetDetails.vue
@@ -0,0 +1,19 @@
+<template>
+    <Base>
+        <button @click="test" class="bg-blue-500 text-white px-4 py-2 rounded">
+            Teste
+        </button>
+    </Base>
+</template>
+<script setup lang="ts">
+import { Asset } from '@/types';
+import Base from '@/layout/Base.vue';
+const props = defineProps<{
+    asset: Asset;
+}>();
+
+const test = () => {
+    console.log('test');
+    console.log(props.asset);
+};
+</script>
