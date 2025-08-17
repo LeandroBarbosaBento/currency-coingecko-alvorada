050cb2f987a8f9d6cacba64663a10c905a0a4f91
leandro <leandro@pop-os.localdomain>
2025-08-17 18:15:30 -0300

    feat: create routes and api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FavoriteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/favorites', [FavoriteController::class, 'store']);
+Route::get('/favorites', [FavoriteController::class, 'index']);
+Route::delete('/favorites/{id}', [FavoriteController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\FavoriteController;
 
 Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
+
+Route::get('/', [AssetController::class, 'index'])->name('assets.index');
+Route::get('/assets/{id}', [AssetController::class, 'show'])->name('assets.show');
+Route::get('/favorites',[FavoriteController::class, 'index'])->name('favorites.index');
