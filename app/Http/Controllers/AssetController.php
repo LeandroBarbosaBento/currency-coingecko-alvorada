2600c1538b6f85949d4b65c0732ee8ff02ddf599
leandro <leandro@pop-os.localdomain>
2025-08-17 18:15:12 -0300

    refactor: controllers

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AssetController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Home');
+    }
+
+    public function show($id)
+    {
+
+    }
+
+
 }
--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class FavoriteController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return Inertia::render('Favorites');
+    }
+
+    public function store(Request $request)
+    {
+        // Logic to store a favorite asset
+    }
+
+    public function destroy($id)
+    {
+        // Logic to remove a favorite asset by ID
+    }
 }
