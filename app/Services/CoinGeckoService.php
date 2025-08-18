15ec7ce08b9d16ccb1402c2ccf8696396a389cb2
leandro <leandro@pop-os.localdomain>
2025-08-18 20:05:12 -0300

    feat: add new parameter to coingecko service to return info from list

--- app/Services/CoinGeckoService.php
+++ app/Services/CoinGeckoService.php
@@ -10,17 +10,23 @@ class CoinGeckoService
     private const BASE_URL = 'https://api.coingecko.com/api/v3/';
     private const CACHE_TTL = 60; // 60 seconds
 
-    public function getMarketList(): array
+    public function getMarketList(array $ids = []): array
     {
-        return Cache::remember('coingecko_assets_list', self::CACHE_TTL, function () {
-            $response = Http::baseUrl(self::BASE_URL)->get('coins/markets', [
-                'vs_currency' => 'usd',
-                'order' => 'market_cap_desc',
-                'per_page' => 10,
-                'page' => 1,
-            ]);
-
-            return $response->json();
+        $query = [
+            'vs_currency' => 'usd',
+            'order' => 'market_cap_desc',
+            'per_page' => 10,
+            'page' => 1,
+        ];
+        if(!empty($ids)) {
+            $query['ids'] = implode(',', $ids);
+        }
+        return Cache::remember(
+            'coingecko_assets_list',
+            self::CACHE_TTL,
+            function () use ($query) {
+                $response = Http::baseUrl(self::BASE_URL)->get('coins/markets', $query);
+                return $response->json();
         });
     }
 
@@ -28,7 +34,6 @@ class CoinGeckoService
     {
         return Cache::remember("coingecko_asset_{$id}", self::CACHE_TTL, function () use ($id) {
             $response = Http::baseUrl(self::BASE_URL)->get("coins/{$id}");
-
             return $response->json();
         });
     }
