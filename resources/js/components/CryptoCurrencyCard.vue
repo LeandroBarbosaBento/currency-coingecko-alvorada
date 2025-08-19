a579bb7c110b872e973ab3da3f81590f2e0c1a23
leandro <leandro@pop-os.localdomain>
2025-08-19 18:10:28 -0300

    feat: add loader on favorite button

--- resources/js/components/CryptoCurrencyCard.vue
+++ resources/js/components/CryptoCurrencyCard.vue
@@ -65,6 +65,7 @@
             class="border-t border-gray-700 mt-4 flex flex-col gap-3"
         >
             <button
+                v-if="!isLoading"
                 class="w-full text-white font-bold py-2 sm:py-3 rounded-lg shadow-md transition duration-300 cursor-pointer"
                 :class="{
                     'bg-red-500 hover:bg-red-600': isFavorite,
@@ -73,6 +74,35 @@
                 @click="onButtonClick(id)"
             >
                 {{ isFavorite ? 'Remove from Favorites' : 'Add to Favorites' }}
+            </button>
+            <button
+                v-else
+                class="w-full bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 sm:py-3 rounded-lg shadow-md transition duration-300 cursor-pointer"
+                disabled
+            >
+                <span class="flex items-center justify-center">
+                    <svg
+                        class="animate-spin h-5 w-5 mr-3"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                        ></circle>
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2.93 6.243A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3.93-1.695z"
+                        ></path>
+                    </svg>
+                    Loading...
+                </span>
             </button>
             <Link
                 class="w-full bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 sm:py-3 rounded-lg shadow-md transition duration-300 cursor-pointer text-center"
@@ -86,7 +116,7 @@
     </div>
 </template>
 <script setup lang="ts">
-import { computed, defineProps } from 'vue';
+import { computed, defineProps, ref } from 'vue';
 import { Link } from '@inertiajs/vue3';
 import {
     markAssetAsFavorite,
@@ -132,6 +162,9 @@ const $emit = defineEmits([
     'marked-as-favorite',
     'remove-from-favorites',
 ]);
+
+const isLoading = ref(false);
+
 const displayedPrice = computed(() => {
     return props.price.toLocaleString('en-US', {
         style: 'currency',
@@ -142,6 +175,7 @@ const displayedPrice = computed(() => {
 
 const onButtonClick = async (id: string) => {
     try {
+        isLoading.value = true;
         if (props.isFavorite) {
             await unmarkAssetAsFavorite(id);
             $emit('remove-from-favorites', id);
@@ -151,6 +185,8 @@ const onButtonClick = async (id: string) => {
         }
     } catch (error) {
         console.error('Error toggling favorite status:', error);
+    } finally {
+        isLoading.value = false;
     }
 };
 
